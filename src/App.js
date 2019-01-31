53bc5e9921aa6b1d17f9e621623cf690c94aef3e
Santhosh Settipalli <santhosh.settipalli@gmail.com>
2019-01-31 13:02:26 +0000

    Imported Bluma. Added textarea and footer to App.js.

--- src/App.js
+++ src/App.js
@@ -5,21 +5,26 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <section className="section">
+          <div className="container">
+            <article className="media">
+              <div className="media-content">
+                <div className="field">
+                  <p className="control">
+                    <textarea name="txtcomment" id="txtcomment" className="textarea" placeholder="Add a comment ..."></textarea>
+                  </p>
+                </div>
+              </div>
+            </article>
+          </div>
+        </section>
+          <hr/>
+        <div className="footer">
+          <div className="content has-text-centered">
+              Copyright (c) Santhoshkumar Settipalli
+          </div>
+        </div>
       </div>
     );
   }
